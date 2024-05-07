649390d4bd92949afefe8710dab35cc2093fc234
Julia Amaral <juliadasilvaamaral23@gmail.com>
2024-05-07 10:29:41 -0300

    Introdução ao JS

---
+++ Unit10/script.js
@@ -0,0 +1,57 @@
+//var frase = "Olá Mundo!"
+//window.alert(5+9)
+
+//boleana
+var chuva = false
+var sol = true
+
+console.log(chuva)
+
+//number
+var idade  = 18
+var altura = 1.70
+
+//string
+var nome = "pedro"
+
+console.log (typeof(chuva))
+console.log(typeof(idade))
+console.log(typeof(nome))
+
+//var = escopo global pode ter seu valor alterado , permite criar variaveis vazias
+ var sobrenome = " silva"
+ sobrenome = "rocha"
+
+ var pessoa  //null
+
+ console.log (pessoa)
+
+ //let = escopo local, pode ter seu valor alterado,permite criar variaveis vazias
+
+ let aluno = "ana"
+ aluno = "bia"
+
+ // const = escopo local,não pode ser alterada, não permite criar variaveis vazias
+
+ const comida = "lasanha"
+
+
+
+ if (true){
+
+    var animal = "gato"
+ }
+
+ console.log (animal)
+
+ document.getElementById("teste").innerHTML = nome
+
+ var comparacao = "0" == 0
+ console.log (comparacao)
+
+ var comparacao2 = "0" === 0
+ console.log (comparacao2)
+
+ 
+ 
+
