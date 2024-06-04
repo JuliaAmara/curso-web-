067f8c873738d2c20240d51d12b14d816ecdfb23
Julia Amaral <juliadasilvaamaral23@gmail.com>
2024-06-04 10:29:39 -0300

    POO

--- Unit12/script.js
+++ Unit12/script.js
@@ -114,8 +114,23 @@ function criarInputs(){
 
     document.getElementById("divInputs").innerHTML = htmlFinal;
 
-
-
 }
+
+
+function  calcularMedia(){
+
+    var inputs = document.getElementsByClassName("inputMedia");
+    var numeroInputs = inputs.length;
+
+    var soma = 0;
+
+    for(var i=0; i < numeroInputs; i++){
+        soma += parseInt(inputs[i].value)
+    }
+
+    document.getElementById("mediaFinal").innerHTML = soma/numeroInputs
+    
+}
+
 
 
---
+++ Unit13/script.js
@@ -0,0 +1,51 @@
+//JQuerry: biblioteca do javascript que permite simplificar a sintaxe
+
+//JavaScript - ID
+//document.getElementById("nomeID");
+
+//JQuerry - ID 
+//$("#nomeID");
+
+//JavaScript - Classe 
+//document.getElementsByClassName("nomeCLasse");
+
+//JQuerry - Classe 
+ //$(".nomeClasse");
+
+ //JavaScript - Tag 
+//document.getElementsByTagName("p");
+
+//JQuerry
+//$("p");
+
+//Seleciona todos os elementos de um HTML
+//$("*");
+
+//Seleciona o primeiro elemento <p></p>
+//$("p:first");
+
+//Seleciona o ultimo elemento <p></p>
+//$("p:last");
+
+//Seleciona todos os elementos com o atributo disabled
+//$("[disabled]")
+
+//Combinando seleções 
+
+//JS 
+//document.getElementById("nomeId").getElementsByTagName('p');
+
+//QJ
+//$("#nomeId p");
+
+
+//Programação orientada a objetos 
+
+var nome = "zezinho"
+var sobrenome = "Silva"
+var idade = 12;
+
+var pessoa = {nome: "zezinho" , sobrenome: "Silva" , idade: 12}
+
+
+
